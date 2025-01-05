f6ae81e29ec15eb604f970148f71f5cce9b1ff1f
Cris <jlfang07@gmail.com>
2025-01-05 14:02:07 +0800

    feat: update env var

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ COPY src /home/app/src
 COPY pom.xml /home/app
 RUN mvn -f /home/app/pom.xml clean package
 
-FROM openjdk:8-jre-slim
+FROM registry.cn-hangzhou.aliyuncs.com/acs-openjdk/openjdk-8-jre-slim
 COPY --from=build /home/app/target/order-demo-1.0.0.jar /usr/local/lib/order-demo.jar
 ENTRYPOINT ["java", "-jar", "/usr/local/lib/order-demo.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker-compose up --build -d
+# docker-compose up --build -d
 
-echo "Waiting for database to be ready..."
-while ! docker exec -it $(docker ps -qf "name=db") mysqladmin ping --silent; do
-  sleep 2
-done
+# echo "Waiting for database to be ready..."
+# while ! docker exec -it $(docker ps -qf "name=db") mysqladmin ping --silent; do
+#   sleep 2
+# done
 
-docker-compose logs -f
+# docker-compose logs -f
